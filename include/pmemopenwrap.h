63c9540a60863a30b94e9cd4f4b46398afb55961
Nakashima <satoshinakashima@sslab.ics.keio.ac.jp>
2022-09-16 07:19:10 +0000

    normak

--- include/pmemopenwrap.h
+++ include/pmemopenwrap.h
@@ -3,11 +3,11 @@
 
 #include <libpmemobj.h>
 #define pmemobj_open pmemobjtest_open
-#define pmemobj_create pmemobjtest_create
+// #define pmemobj_create pmemobjtest_create
 
-#define pmemobj_persist pmemobjtest_persist
-#define pmemobj_tx_process pmemobjtest_tx_process
-#define pmemobj_close pmemobjtest_close
+// #define pmemobj_persist pmemobjtest_persist
+// #define pmemobj_tx_process pmemobjtest_tx_process
+// #define pmemobj_close pmemobjtest_close
 
 extern inline PMEMobjpool *pmemobjtest_open(const char *path, const char *layout);
 
--- main/PmemKVS.c
+++ main/PmemKVS.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <libpmemobj.h>
-#include "pmemopenwrap.h"
+//#include "pmemopenwrap.h"
 
 // Name of our layout in the pool
 #define LAYOUT "KVS_layout"
@@ -21,6 +21,9 @@
 // #define PATH "/mnt/pmem0/data_replica"
 //#define NUM_PATH "num"
 
+#define READ_FROM_KEY 0
+#define READ_ALL 1
+#define READ_FROM_ID 2
 
 // Root structure
 struct subKVstruct {
@@ -100,7 +103,7 @@ void write_KVS (char *key, int value)
 /****************************
  * This function reads the "Hello..." string from persistent-memory.
  *****************************/
-void read_KVS(char *key, int read_all)
+void read_KVS(char *key, int read_flag, int id)
 {
     PMEMobjpool *pop;
     int number;
@@ -120,7 +123,7 @@ void read_KVS(char *key, int read_all)
 
     int flag = 0;
     int i;
-    if(read_all == 0){
+    if(read_flag == READ_FROM_KEY){
         for(i = 0; i < number; i++){
             printf("loop: %d\n", i);
             if(strncmp(D_RO(root)->data[i].key, key, D_RO(root)->data[i].keylen) == 0){
@@ -132,10 +135,13 @@ void read_KVS(char *key, int read_all)
         if(flag == 0)
             printf("%s is not registered.\n", key);
     }
-    else{
+    else if(read_flag == READ_ALL){
         for(i = 0; i < number; i++){
             printf("Read [key: %s, value: %d]\n", D_RO(root)->data[i].key, D_RO(root)->data[i].value);
         }
+    }
+    else{
+        printf("Read [key: %s, value: %d]\n", D_RO(root)->data[id].key, D_RO(root)->data[id].value);
     }
     
 	pmemobj_close(pop);
@@ -185,6 +191,71 @@ void delete_KVS(char *key){
 	return;
 }
 
+void manipulate_data(int id, char *key, int value){
+    PMEMobjpool *pop;
+    TOID(struct KVstruct) root;
+    TOID(struct subKVstruct) sub;
+    
+    pop = pmemobj_create(PATH, POBJ_LAYOUT_NAME(string_store), PMEMOBJ_MIN_POOL, 0666);
+		
+	if (pop == NULL) 
+	{
+        pop = pmemobj_open(PATH, POBJ_LAYOUT_NAME(string_store));
+
+        if(pop == NULL){
+            perror(PATH);
+            exit(1);
+        }
+    }
+
+    root = POBJ_ROOT(pop, struct KVstruct);
+
+    TX_BEGIN(pop){
+        TX_ADD(root);
+	    D_RW(root)->data[id].keylen = strlen(key);
+        D_RW(root)->data[id].value = value;
+        TX_MEMCPY(D_RW(root)->data[id].key, key, D_RO(root)->data[id].keylen);
+    } TX_END
+
+	printf("Manipulate [id: %d, key: %s, value: %d]\n", id, D_RO(root)->data[id].key, D_RO(root)->data[id].value);
+    printf("number : %d\n", D_RO(root)->number);
+
+	pmemobj_close(pop);	
+		
+	return;
+}
+
+void manipulate_number(int number){
+    PMEMobjpool *pop;
+    TOID(struct KVstruct) root;
+    TOID(struct subKVstruct) sub;
+    
+    pop = pmemobj_create(PATH, POBJ_LAYOUT_NAME(string_store), PMEMOBJ_MIN_POOL, 0666);
+		
+	if (pop == NULL) 
+	{
+        pop = pmemobj_open(PATH, POBJ_LAYOUT_NAME(string_store));
+
+        if(pop == NULL){
+            perror(PATH);
+            exit(1);
+        }
+    }
+
+    root = POBJ_ROOT(pop, struct KVstruct);
+
+    TX_BEGIN(pop){
+        TX_ADD(root);
+	    D_RW(root)->number = number;
+    } TX_END
+    
+    printf("number : %d\n", D_RO(root)->number);
+
+	pmemobj_close(pop);	
+		
+	return;
+}
+
 /****************************
  * This main function gather from the command line and call the appropriate
  * function to perform read and write persistently to memory.
@@ -197,11 +268,19 @@ int main(int argc, char *argv[])
         int value = atoi(argv[3]);
 		write_KVS(key, value);
 	} else if (strcmp (argv[1], "-r") == 0 && argc == 3) {
-		read_KVS(key, 0);
+		read_KVS(key, READ_FROM_KEY, -1);
 	} else if (strcmp (argv[1], "-ra") == 0 && argc == 2) {
-		read_KVS("", 1);
+		read_KVS("", READ_ALL, -1);
+    } else if (strcmp (argv[1], "-ri") == 0 && argc == 3) {
+		read_KVS("", READ_FROM_ID, atoi(argv[2]));
 	} else if (strcmp (argv[1], "-d") == 0 && argc == 3){
         delete_KVS(key);
+    } else if (strcmp (argv[1], "-md") == 0 && argc == 5){
+        //-md number key value
+        manipulate_data(atoi(argv[2]), argv[3], atoi(argv[4]));
+    } else if (strcmp (argv[1], "-mn") == 0 && argc == 3){
+        //-mn number
+        manipulate_number(atoi(argv[2]));
     } else { 
 		fprintf(stderr, "Usage error: %s\n", argv[0]);
 		exit(1);
